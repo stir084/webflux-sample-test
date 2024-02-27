4ea08396b08c4d44ce4bec37e8b61e54fc535966
junho <wnsgh1357@gmail.com>
2024-02-28 01:02:12 +0900

    feat: video streaming feature, access localhost:9000

---
+++ src/main/java/com/example/webfluxsampletest/video/StreamingController.java
@@ -0,0 +1,22 @@
+package com.example.webfluxsampletest.video;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+import org.springframework.core.io.Resource;
+
+@RestController
+public class StreamingController {
+
+    @Autowired
+    private StreamingService service;
+
+    @GetMapping(value = "video/{title}", produces = "video/mp4")
+    public Mono<Resource> getVideos(@PathVariable String title, @RequestHeader("Range") String range) {
+        System.out.println("range in bytes() : " + range);
+        return service.getVideo(title);
+    }
+}
